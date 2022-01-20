c08f1ad820384bb7e9b36d4cdc3edcfdf37895cd
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-01-20 22:06:47 +0300

    Footer Component

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,23 +3,21 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 function Navbar({ title }) {
   return (
-    <div className='navbar shadow-lg bg-neutral mb-12 text-neutral-content'>
+    <div className='navbar mb-2 shadow-lg bg-neutral text-neutral-content'>
       <div className='container mx-auto'>
-        <div className='flex-none px-2 mx-2'>
-          <Link to='/' className='text-lg font-bold'>
-            <FaGithub className='inline pr-2 text-4xl' />
+        <div className='flex-1 px-2 mx-2'>
+          <Link to='/' className='flex items-center text-lg font-bold'>
+            <FaGithub className='mx-2 text-4xl' />
             {title}
           </Link>
         </div>
-        <div className='flex-1 px-2 mx-2'>
-          <div className='flex justify-end'>
-            <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
-              Home
-            </Link>
-            <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
-              About
-            </Link>
-          </div>
+        <div className='px-2 mx-2 '>
+          <Link to='/' className='btn btn-ghost font-bold'>
+            Home
+          </Link>
+          <Link to='/about' className='btn btn-ghost font-bold'>
+            About
+          </Link>
         </div>
       </div>
     </div>
