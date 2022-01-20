9b2f293aaec1f6cf3a7c677abdc492bfc084eea2
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-01-20 19:49:22 +0300

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-      <button className='inline-block px-4 py-3 text-sm font-semibold text-center text-white uppercase transition duration-200 ease-in-out bg-indigo-500 rounded-md cursor-pointer hover:bg-indigo-600'>
-        Tailwind Button
-      </button>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main>Content</main>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,37 @@
+import { FaGithub } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+function Navbar({ title }) {
+  return (
+    <div className='navbar shadow-lg bg-neutral mb-12 text-neutral-content'>
+      <div className='container mx-auto'>
+        <div className='flex-none px-2 mx-2'>
+          <Link to='/' className='text-lg font-bold'>
+            <FaGithub className='inline pr-2 text-4xl' />
+            {title}
+          </Link>
+        </div>
+        <div className='flex-1 px-2 mx-2'>
+          <div className='flex justify-end'>
+            <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
+              Home
+            </Link>
+            <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
+              About
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Navbar.defaultProps = {
+  title: 'Github Finder',
+};
+
+Navbar.protoType = {
+  title: PropTypes.string,
+};
+
+export default Navbar;
